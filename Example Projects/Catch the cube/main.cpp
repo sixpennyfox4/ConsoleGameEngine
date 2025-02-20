c4144627796e714a2cae5179b3d32597b603506c
sixpennyfox4 <asenoprenov2022@gmail.com>
2025-02-20 22:36:08 +0200

    Create main.cpp

---
+++ Example Projects/Catch the cube/main.cpp
@@ -0,0 +1,40 @@
+// Example Project 1 (catch the cube)
+// Used V0.1
+
+#include "ConsoleGameEngine.hpp"
+
+using namespace CGE;
+
+int main() {
+	int score{ 0 };
+
+	CG game;
+	game.InitConsoleSpace(40, 80, "Catch the cube");
+
+	Rectangle plr{ Vector2{ 20, 10 }, 5, 5 };
+	Rectangle movingRec{ Vector2{ getRandomNumber(10, 75), getRandomNumber(10, 35) }, 5, 5 };
+	while (true) {
+		// We will use GetAsyncKeyState which isnt good but if it works it works.
+		if (GetAsyncKeyState('W')) plr.position.y -= 2;
+		if (GetAsyncKeyState('S')) plr.position.y += 2;
+		if (GetAsyncKeyState('A')) plr.position.x -= 2;
+		if (GetAsyncKeyState('D')) plr.position.x += 2;
+
+		game.drawOutline(COLOR_GREEN);
+		game.drawText(Vector2{ 5, 20 }, score);
+		game.drawRectangleRec(movingRec, true, COLOR_RED);
+		game.drawRectangleRec(plr, true, COLOR_BLUE);
+
+		if (game.checkCollisionRecs(plr, movingRec)) {
+			score++;
+
+			movingRec.position = Vector2{ getRandomNumber(10, 75), getRandomNumber(10, 35) };
+			plr.position = Vector2{ 20, 10 };
+		}
+
+		wait(100);
+		game.clearConsole();
+	}
+
+	return 0;
+}
